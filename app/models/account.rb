1ce26af9709dcdff4ad01f05924b8df08033fbed
Yan Fernandes <yanfernandes@gmail.com>
2022-11-16 01:06:45 -0300

    build model relations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,8 @@
 class Account < ApplicationRecord
   belongs_to :user
   belongs_to :bank
+
+  has_many :deposits
+  has_many :received_transfers, foreign_key: :receiver_id, class_name: 'Transfer'
+  has_many :sent_transfers, foreign_key: :sender_id, class_name: 'Transfer'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :accounts
+  has_many :banks, through: :accounts
+
+  validates :username, {presence: true, uniqueness: true}
+  validates :password, {presence: true, length: { in: 6..10 }}
+  validates_confirmation_of :password
+  validates_presence_of :password_confirmation, { case_sensitive: true}
 end
