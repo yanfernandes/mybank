82191bfcac12dcc8802816622660498ee96f2f04
Yan Fernandes <yanfernandes@gmail.com>
2022-11-16 11:25:37 -0300

    fix model methods

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,10 +4,25 @@ class Account < ApplicationRecord
 
   has_many :deposits
   has_many :received_transfers, foreign_key: :receiver_id, class_name: 'Transfer'
+  has_many :receivers, through: :received_transfers
+
   has_many :sent_transfers, foreign_key: :sender_id, class_name: 'Transfer'
+  has_many :senders, through: :sent_transfers
 
   def username
     User.find(self.user_id).username
+  end
+
+  def self.get_all_users_with_banks
+    accounts = Account.all
+    usernames_and_banks = accounts.map do |account|
+      User.find(account.user_id).username + " - " + Bank.find(account.bank_id).name
+    end
+    usernames_and_banks
+  end
+
+  def get_user_with_bank
+    User.find(self.user_id).username + " - " + Bank.find(self.bank_id).name
   end
 
   def self.sorted_accounts
