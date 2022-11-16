3dd71b7c83b7dab9fc24213cc2788e8727bdff3d
Yan Fernandes <yanfernandes@gmail.com>
2022-11-16 13:42:08 -0300

    build withdraw

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -1,13 +1,12 @@
 class WithdrawsController < ApplicationController
-  before_action :set_withdraw, only: %i[ show edit update destroy ]
-
   # GET /withdraws or /withdraws.json
   def index
-    @withdraws = Withdraw.all
+    @withdraws = Withdraw.where(account: Account.find(params[:account_id]))
   end
 
   # GET /withdraws/1 or /withdraws/1.json
   def show
+    @withdraw = Withdraw.find(params[:id])
   end
 
   # GET /withdraws/new
@@ -21,50 +20,16 @@ class WithdrawsController < ApplicationController
 
   # POST /withdraws or /withdraws.json
   def create
-    @withdraw = Withdraw.new(withdraw_params)
-
-    respond_to do |format|
-      if @withdraw.save
-        format.html { redirect_to withdraw_url(@withdraw), notice: "Withdraw was successfully created." }
-        format.json { render :show, status: :created, location: @withdraw }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @withdraw.errors, status: :unprocessable_entity }
-      end
-    end
+    @withdraw = Withdraw.create(bank_id: Bank.find(Account.find(params[:account_id]).bank_id).id, account_id: params[:account_id], amount: params[:withdraw][:amount])
+    @withdraw.fulfill_withdraw
+    redirect_to user_account_path(session[:user_id], @withdraw.account_id)
   end
 
   # PATCH/PUT /withdraws/1 or /withdraws/1.json
   def update
-    respond_to do |format|
-      if @withdraw.update(withdraw_params)
-        format.html { redirect_to withdraw_url(@withdraw), notice: "Withdraw was successfully updated." }
-        format.json { render :show, status: :ok, location: @withdraw }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @withdraw.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /withdraws/1 or /withdraws/1.json
   def destroy
-    @withdraw.destroy
-
-    respond_to do |format|
-      format.html { redirect_to withdraws_url, notice: "Withdraw was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_withdraw
-      @withdraw = Withdraw.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def withdraw_params
-      params.require(:withdraw).permit(:account_id, :bank_id, :amount)
-    end
 end
--- app/models/withdraw.rb
+++ app/models/withdraw.rb
@@ -1,4 +1,16 @@
 class Withdraw < ApplicationRecord
   belongs_to :account
   belongs_to :bank
+
+  def fulfill_withdraw
+    self.account.update(balance: self.account.balance - self.amount)
+  end
+
+  def print_interest_rate
+    self.interest_rate.truncate(3).to_s + "%"
+  end
+
+  def print_bank
+    Bank.find(self.bank_id).name
+  end
 end
